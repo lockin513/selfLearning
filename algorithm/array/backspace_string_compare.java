f5696057dde1df2da5f7e0b7d88bd800391fc0c9
lockin513 <1459615110@qq.com>
2024-07-25 11:24:04 +0800

    7-25删除元素复习

--- algorithm/array/backspace_string_compare.java
+++ algorithm/array/backspace_string_compare.java
@@ -1,31 +1,19 @@
 public class backspace_string_compare {
-    public boolean backspaceCompare(String s, String t) {
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='#' && sb.length()>0)
+    class Solution {
+        public boolean backspaceCompare(String s, String t) {
+            StringBuilder str1 = new StringBuilder();
+            StringBuilder str2 = new StringBuilder();
+            for(int i=0;i<s.length();i++)
             {
-                sb.deleteCharAt(sb.length()-1);
+                if(s.charAt(i)=='#' && !str1.isEmpty()) str1.deleteCharAt(str1.length()-1);
+                else if(s.charAt(i)!='#')str1.append(s.charAt(i));
             }
-            else if(s.charAt(i)!='#')
+            for(int i=0;i<t.length();i++)
             {
-                sb.append(s.charAt(i));
+                if(t.charAt(i)=='#' && !str2.isEmpty())str2.deleteCharAt(str2.length()-1);
+                else if(t.charAt(i)!='#')str2.append(t.charAt(i));
             }
+            return str1.toString().equals(str2.toString());
         }
-        String s1 = sb.toString();
-        for(int i=0;i<t.length();i++)
-        {
-            if(t.charAt(i)=='#' && sb2.length()>0)
-            {
-                sb2.deleteCharAt(sb2.length()-1);
-            }
-            else if(t.charAt(i)!='#')
-            {
-                sb2.append(t.charAt(i));
-            }
-        }
-        String s2 = sb2.toString();
-        return s1.equals(s2);
     }
 }
