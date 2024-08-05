5d67e18e736c00f48bc532a33bbfb36f4cde8edc
lockin513 <1459615110@qq.com>
2024-08-05 10:16:09 +0800

    8-5二叉树复习

--- algorithm/binaryTree/invert_binary_tree.java
+++ algorithm/binaryTree/invert_binary_tree.java
@@ -1,11 +1,16 @@
 public class invert_binary_tree {
-    public TreeNode invertTree(TreeNode root) {
-        if(root==null)return root;
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-        if(root.left!=null)invertTree(root.left);
-        if(root.right!=null)invertTree(root.right);
-        return root;
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            invert(root);
+            return root;
+        }
+        public void invert(TreeNode root){
+            if(root==null)return;
+            if(root.left!=null)invertTree(root.left);
+            if(root.right!=null)invertTree(root.right);
+            TreeNode temp = root.left;
+            root.left=root.right;
+            root.right=temp;
+        }
     }
 }
