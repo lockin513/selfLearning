92b9b4f84a66011a30711a35756a7759683c543f
lockin513 <1459615110@qq.com>
2024-08-12 10:40:57 +0800

    8-12二叉树复习

--- algorithm/binaryTree/insert_into_a_binary_search_tree.java
+++ algorithm/binaryTree/insert_into_a_binary_search_tree.java
@@ -1,22 +1,20 @@
 public class insert_into_a_binary_search_tree {
-    public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null)return new TreeNode(val);
-        TreeNode ans = root;
-        while(true)
-        {
-            if(root.val>val)
-                if(root.left!=null)root = root.left;
-                else {
-                    root.left = new TreeNode(val);
-                    break;
-                }
-            if(root.val<val)
-                if(root.right!=null)root = root.right;
-                else{
-                    root.right = new TreeNode(val);
-                    break;
-                }
+    class Solution {
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+            if(root==null)return new TreeNode(val);
+            insert(root,val);
+            return root;
         }
-        return ans;
+        public void insert(TreeNode root,int val){
+            if(root.val>val)
+            {
+                if(root.left!=null)insert(root.left,val);
+                else root.left = new TreeNode(val);
+            }
+            else{
+                if(root.right!=null)insert(root.right,val);
+                else root.right = new TreeNode(val);
+            }
+        }
     }
 }
