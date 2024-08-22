4e11a4e656cb5fed8e75b71eccb743a3f8917a4c
lockin513 <1459615110@qq.com>
2024-08-22 10:28:08 +0800

    8-22贪心复习

--- algorithm/greed/lemonade_change.java
+++ algorithm/greed/lemonade_change.java
@@ -1,39 +1,30 @@
 public class lemonade_change {
     class Solution {
         public boolean lemonadeChange(int[] bills) {
-            int five = 0;
-            int ten = 0;
-            int flag = 1;
+            int five=0,ten=0;
             for(int i=0;i<bills.length;i++)
             {
                 if(bills[i]==5)five++;
                 else if(bills[i]==10)
                 {
-                    if(five!=0)
+                    if(five>0)
                     {
                         five--;
                         ten++;
-                    }
-                    else {
-                        flag = 0;
-                        break;
-                    }
+                    }else return false;
                 }
-                else{
-                    if(five>=1 && ten>=1)
+                else
+                {
+                    if(ten>0 && five>0)
                     {
                         five--;
                         ten--;
                     }
-                    else if(five>=3) five-=3;
-                    else{
-                        flag = 0;
-                        break;
-                    }
+                    else if(five>=3)five-=3;
+                    else return false;
                 }
             }
-            if(flag==1)return true;
-            else return false;
+            return true;
         }
     }
 }
