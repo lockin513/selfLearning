5531ebad1bdcf026fd96eb3a19df033640995633
lockin513 <1459615110@qq.com>
2024-08-24 15:43:35 +0800

    8-24多线程复习

--- javaLearning/Thread/D8ThreadWaitAndNotify/Cook.java
+++ javaLearning/Thread/D8ThreadWaitAndNotify/Cook.java
@@ -4,17 +4,17 @@ public class Cook extends Thread{
     @Override
     public void run() {
         while(true){
-            synchronized (Desk.lock){
+            synchronized(Desk.lock){
                 if(Desk.count==0)break;
                 else{
-                    if(Desk.foodflag==1){
+                    if(Desk.foodflag==1) {
                         try {
                             Desk.lock.wait();
                         } catch (Exception e) {
                             throw new RuntimeException(e);
                         }
-                    }else {
-                        System.out.println("厨师正在做面条");
+                    }else{
+                        System.out.println("厨师做好了一碗面");
                         Desk.foodflag=1;
                         Desk.lock.notifyAll();
                     }
--- javaLearning/Thread/D8ThreadWaitAndNotify/Foodie.java
+++ javaLearning/Thread/D8ThreadWaitAndNotify/Foodie.java
@@ -4,22 +4,20 @@ package Thread.D8ThreadWaitAndNotify;
 public class Foodie extends Thread{
     @Override
     public void run() {
-        while (true){
+        while(true){
             synchronized (Desk.lock){
                 if(Desk.count==0)break;
-                else
-                {
-                    if(Desk.foodflag==0){
+                else {
+                    if (Desk.foodflag == 0) {
                         try {
                             Desk.lock.wait();
                         } catch (Exception e) {
                             throw new RuntimeException(e);
                         }
-                    }
-                    else
-                    {
-                        System.out.println("吃货正在吃面条,还能再吃"+--Desk.count+"碗");
-                        Desk.foodflag=0;
+                    } else {
+                        Desk.count--;
+                        System.out.println("吃货正在吃面条，还能再吃" + Desk.count + "碗");
+                        Desk.foodflag = 0;
                         Desk.lock.notifyAll();
                     }
                 }
