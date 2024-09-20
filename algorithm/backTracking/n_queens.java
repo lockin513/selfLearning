7c331c94bb82f888212faa2e721432cd8473b005
lockin513 <1459615110@qq.com>
2024-09-20 10:42:16 +0800

    9-20回溯复习完毕

--- algorithm/backTracking/n_queens.java
+++ algorithm/backTracking/n_queens.java
@@ -4,43 +4,54 @@ import java.util.List;
 
 public class n_queens {
     class Solution {
-        public List<List<String>> ans = new ArrayList<>();
+        int[][] board = new int[9][9];
+        List<List<String>> ans = new ArrayList<>();
         public List<List<String>> solveNQueens(int n) {
-            char[][] chessboard = new char[n][n];
-            for (char[] c : chessboard) {
-                Arrays.fill(c, '.');
-            }
-            backTracking(n,0,chessboard);
+            backTracking(n,0);
             return ans;
         }
-        public void backTracking(int n, int row, char[][] chessboard){
-            if(row==n){
-                ans.add(new ArrayList<>(Array2List(chessboard)));
+        public void backTracking(int n,int startIndex){
+            if(startIndex==n)
+            {
+                ans.add(new ArrayList<>(board2ArrayList(n)));
                 return;
             }
-            for(int col=0;col<n;col++)
+            for(int i=0;i<n;i++)
             {
-                chessboard[row][col]='Q';
-                if(isValid(n,row,col,chessboard))backTracking(n,row+1,chessboard);
-                chessboard[row][col]='.';
+                if(isValid(n,startIndex,i))
+                {
+                    board[startIndex][i]=1;
+                    backTracking(n,startIndex+1);
+                    board[startIndex][i]=0;
+                }
             }
         }
-        public boolean isValid(int n, int row, int col, char[][] chessboard){
-            for(int i=0;i<row;i++)
-                if(chessboard[i][col]=='Q')return false;
-            for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-                if(chessboard[i][j]=='Q')return false;
-            }
-            for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++)
-                if(chessboard[i][j]=='Q')return false;
+        public boolean isValid(int n,int row,int col){
+            //判断这一列是否可以放
+            for(int i=0;i<n;i++)
+                if(board[i][col]==1)return false;
+            //判断这一行是否可以放
+            for(int i=0;i<n;i++)
+                if(board[row][i]==1)return false;
+            //判断左上斜线是否可以放
+            for(int i=row,j=col;i>=0 && j>=0;i--,j--)
+                if(board[i][j]==1)return false;
+            //判断右上斜线是否可以放
+            for(int i=row,j=col;i>=0 && j<n;i--,j++)
+                if(board[i][j]==1)return false;
             return true;
         }
-        public List Array2List(char[][] chessboard) {
-            List<String> list = new ArrayList<>();
-            for (char[] c : chessboard) {
-                list.add(String.copyValueOf(c));
+        public List<String> board2ArrayList(int n){
+            List<String> res = new ArrayList<>();
+            for(int i=0;i<n;i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                for(int j=0;j<n;j++)
+                    if(board[i][j]==1)sb.append('Q');
+                    else sb.append('.');
+                res.add(sb.toString());
             }
-            return list;
+            return res;
         }
     }
 }
