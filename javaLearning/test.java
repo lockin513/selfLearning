d6adef99d2a1c27adbb545272f3219adc4ae32a2
lockin513 <1459615110@qq.com>
2024-09-22 14:09:04 +0800

    9-22亚马逊笔试练习

--- javaLearning/test.java
+++ javaLearning/test.java
@@ -1,11 +1,44 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 class test {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in); // Create a Scanner object named in.
-        int a = in.nextInt(); // let in read a integer from command line and assign the value to variable a.
-        System.out.println("Your input integer is: " + a);  // Display "Your input integer is: " and variable a.
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        List<List<Integer>> sortedList = groupSort(list);
+        for (List<Integer> subList : sortedList) {
+            System.out.println(subList);
+        }
+    }
+
+    public static List<List<Integer>> groupSort(List<Integer> arr) {
+        int[] rem = new int[100010];
+        for (int i = 0; i < arr.size(); i++) {
+            rem[arr.get(i)]++; // 统计频率
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < rem.length; i++) {
+            if (rem[i] != 0) {
+                List<Integer> con = new ArrayList<>();
+                con.add(i);
+                con.add(rem[i]);
+                ans.add(con);
+            }
+        }
+
+        Collections.sort(ans, (a, b) -> {
+            if (a.get(1) != b.get(1)) {
+                return b.get(1) - a.get(1); // 频率降序
+            } else {
+                return a.get(0) - b.get(0); // 值升序
+            }
+        });
+
+        return ans;
     }
 }
