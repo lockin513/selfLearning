f10e1f21f1d8a032dd30837ec5a3f80a29651fef
lockin513 <1459615110@qq.com>
2024-09-24 13:53:35 +0800

    9-24练习

--- javaLearning/test.java
+++ javaLearning/test.java
@@ -1,13 +1,20 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 class test {
     public static void main(String[] args) {
-        System.out.println(add(1,0.1));
-        System.out.println(add(0.2,0.2));
-    }
-    public static <T extends Number> double add(T num1, T num2) {
-        return num1.doubleValue() + num2.doubleValue();
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> list2 = new ArrayList<>();
+        list.add(1);
+        list2.add(list);
+        for (List<Integer> integers : list2) {
+            System.out.println(integers);
+        }
+        list.add(2);
+        for (List<Integer> integers : list2) {
+            System.out.println(integers);
+        }
     }
 }
